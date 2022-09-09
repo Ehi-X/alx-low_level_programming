66318f01b9db6e8df7b8e2f694dcaf04ef8c4509
Ehis Christopher <chrisehist@gmail.com>
2022-09-09 15:22:56 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,18 +11,26 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-		for (l = 50; l < 58; l++)
+
+		for (m = 49; m < 58; m++)
 		{
-			if (l > m && m > n)
-			{
-				putchar(n);
-				putchar(m);
-				putchar(l);
-				if (n != 55 || m != 56)
+
+			for (l = 50; l < 58; l++)
+			{	
+
+				if (l > m && m > n)
 				{
-					putchar(',');
-					putchar(' ');
-				}
+
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					
+					}
+				}	
 			}
 		}
 	}
